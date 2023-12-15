e0431395bd539d6bbf622461da008d0781ccd319
Enzo Deshayes <enzo.deshayes10@icloud.com>
2023-12-15 02:03:20 +0100

    testé

--- src/logging.c
+++ src/logging.c
@@ -5,11 +5,13 @@
 #include "logging.h"
 #include <stdio.h>
 
-void logPacket(const char *packetData, int packetSize) {
+void logPacket(const void *data, int length) {
     FILE *logFile = fopen("packet_log.txt", "a");
     if (logFile != NULL) {
-        fprintf(logFile, "Packet Size: %d, Data: %s\n", packetSize, packetData);
+        fprintf(logFile, "Packet Length: %d, Data: %.*s\n", length, length, (const char *)data);
         fclose(logFile);
+    } else {
+        // Vous pouvez ajouter un message d'erreur ici si le fichier ne peut pas être ouvert
+        perror("Error opening log file");
     }
 }
-
